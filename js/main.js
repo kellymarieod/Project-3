3e78a96ce1ffe1d60d03f86cf641dd0b5a367616
kellymarieod <kellymarieod@gmail.com>
2016-04-13 13:46:31 -0400

    Updated the JQuery convo

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@
 
 $(document).ready( function() {
   $(".sendf").click( function() {
-    $(".convocontent").append(" <a href='Scene2/Scene2.html' class='her'>Sup</a>");
+    $(".convocontent").append(" <a href='Scene2/Scene2.html' class='her'>When will you be in town again? It'd be great to see you sometime.</a>");
   });
   $(".sendm").click( function() {
-    $(".convocontent").append(" <a href='Scene2/Scene2.html' class='him'>Hi</a>");
+    $(".convocontent").append(" <a href='Scene2/Scene2.html' class='him'>So... I'm coming home next week for the long weekend. Want to grab a cup of coffee?</a>");
   });
 });
