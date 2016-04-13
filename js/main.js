7edefac0c65abf06040865064eb7c20794245f8e
kellymarieod <kellymarieod@gmail.com>
2016-04-13 15:15:36 -0400

    applied CSS to JQuery convo input

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@
 
 $(document).ready( function() {
   $(".sendf").click( function() {
-    $(".convocontent").append(" <a href='Scene2/Scene2.html' class='her'>When will you be in town again? It'd be great to see you sometime.</a>");
+    $(".convocontent").append(" <a href='Scene2/Scene2.html'><p class='her'>When will you be in town again? It'd be great to see you sometime.</p></a>");
   });
   $(".sendm").click( function() {
-    $(".convocontent").append(" <a href='Scene2/Scene2.html' class='him'>So... I'm coming home next week for the long weekend. Want to grab a cup of coffee?</a>");
+    $(".convocontent").append(" <a href='Scene2/Scene2.html'><p class='him'>So... I'm coming home next week for the long weekend. Want to grab a cup of coffee?</p></a>");
   });
 });
